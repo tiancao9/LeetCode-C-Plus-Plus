2f65f67787853cacd59feadd492424e041ef8a7b
WEIBO <weibo@WEIBOs-MacBook.attlocal.net>
2018-03-20 23:02:59 -0700

    add_208

---
+++ 208-Implement_Trie_Prefix_Tree.cpp
@@ -0,0 +1,66 @@
+/*
+Implement a trie with insert, search, and startsWith methods.
+
+Note:
+You may assume that all inputs are consist of lowercase letters a-z.
+*/
+
+//define TrieNode structure, each node has a array[26] pointer to 26 letter child, also record if there is a word ending in the node
+
+struct TrieNode{
+    TrieNode* child[26];
+    bool isWord;
+    TrieNode() : isWord(false){
+        for(auto &a: child) a = NULL;}
+    };
+class Trie {
+public:
+    /** Initialize your data structure here. */
+    
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        TrieNode *p = root;
+        for (char a : word) {
+            int i = a - 'a';
+            if (!p->child[i]) p->child[i] = new TrieNode();
+            p = p->child[i];
+        }
+        p->isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        TrieNode *p = root;
+        for (char a :word) {
+            int i = a - 'a';
+            if (!p->child[i]) return false;
+            p = p->child[i];
+        }
+        return p->isWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        TrieNode *p = root;
+        for (char a : prefix) {
+            int i = a - 'a';
+            if (!p->child[i]) return false;
+            p = p->child[i];
+        }
+        return true;
+    }
+private:
+    TrieNode * root;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * bool param_2 = obj.search(word);
+ * bool param_3 = obj.startsWith(prefix);
+ */
