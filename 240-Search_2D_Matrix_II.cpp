096e09dd3d65b5068723660aed13aed260c568a1
WEIBO <weibo@WEIBOs-MacBook.attlocal.net>
2018-04-16 21:10:00 -0700

    add-240

---
+++ 240-Search_2D_Matrix_II.cpp
@@ -0,0 +1,44 @@
+/*
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+For example,
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+*/
+// Search from right top corner, if target == matrix[row][col] return True, if Target > matrix[row][col] row += 1, else, col -= 1
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+            
+        if(matrix.empty() || matrix[0].empty())
+            return 0;
+        
+        int row = 0;
+        int col = matrix[0].size()-1;
+        
+        while (row < matrix.size() && col >= 0)
+        {
+            if(matrix[row][col] == target)
+                return 1;
+            else if(matrix[row][col] < target)
+                row++;
+            else
+                col--;
+                    
+        }
+        return 0;
+    }
+};
