d595d4a95c41b0d246b6717b522b114df8ff5e6f
WEIBO <weibo@WEIBOs-MacBook.attlocal.net>
2018-05-14 21:04:27 -0700

    add-155

---
+++ 155-Min_Stack.cpp
@@ -0,0 +1,59 @@
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+Example:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+*/
+//use 2 stack
+
+class MinStack {
+ 
+
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+    
+    }
+    
+    void push(int x) {
+        s.push(x);
+        if(min.empty() || x <= min.top()) min.push(x);
+    }
+    
+    void pop() {
+        if(s.top() == min.top())
+            min.pop();
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return min.top();
+    }
+private:
+    stack<int> s;
+    stack<int> min;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
