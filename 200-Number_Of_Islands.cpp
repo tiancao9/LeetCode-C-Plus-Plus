b179233c8b967b17b00fd76047a910ea26e496c2
WEIBO <weibo@WEIBOs-MacBook.attlocal.net>
2018-05-20 11:49:20 -0700

    add-200

--- 200-Number_Of_Islands.cpp
+++ 200-Number_Of_Islands.cpp
@@ -18,7 +18,7 @@ Example 2:
 Answer: 3
 */
 
-//DFS, get '1' and check all its neighbors, mark to '0' if they are '1'
+/* python sol, DFS, get '1' and check all its neighbors, mark to '0' if they are '1'
 class Solution:
     def dfs_1_to_0(self, grid, i, j):
         """
@@ -48,4 +48,42 @@ class Solution:
                     self.dfs_1_to_0(grid, i, j)
                     islands += 1
         return islands
+*/
+
+
+//c++ sol
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int res = 0;
+        if(grid.empty() || grid[0].empty())
+            return res;
+        for(int i = 0; i < grid.size(); i++)
+            for(int j = 0; j < grid[0].size(); j++)
+            {
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    res++;
+                }   
+            }
         
+        return res;
+    }
+
+private:
+    void dfs(vector<vector<char>>& grid, int i, int j)
+    {
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0') return;
+        else 
+        {
+            grid[i][j] = '0';
+            dfs(grid, i+1, j);
+            dfs(grid, i-1, j);
+            dfs(grid, i, j+1);
+            dfs(grid, i, j-1);
+        }
+            
+    }
+  
+};
