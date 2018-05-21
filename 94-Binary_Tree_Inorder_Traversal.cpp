fe65609c3bf00cb4ff0e223833dcaa8da8e54a5c
WEIBO <weibo@WEIBOs-MacBook.attlocal.net>
2018-05-20 21:09:36 -0700

    add-94

---
+++ 94-Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,67 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    
+
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+     stack<TreeNode*> s;
+     vector<int> result;
+     
+        
+     if(root == NULL)
+         return result;
+     else
+         s.push(root);
+        
+     while(root->left)
+     {
+         s.push(root->left);
+         root = root->left;
+     }
+        
+     while(!s.empty())
+     {
+         TreeNode* cur;
+         cur = s.top();
+         s.pop();
+         result.push_back(cur->val);
+         
+         if(cur->right)
+         {
+             s.push(cur->right);
+             cur = cur->right;
+             while(cur->left)
+             {
+                 s.push(cur->left);
+                 cur = cur->left;
+             }             
+         }         
+     }
+        
+        return result;
+        
+    }        
+};
