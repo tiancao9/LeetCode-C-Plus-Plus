5ebe5740e4b1f96ebe1bc3f0cb683e8422212948
WEIBO <weibo@WEIBOs-MacBook.local>
2018-06-03 22:04:54 -0700

    add-739

---
+++ 739-Daily_Temperatures.cpp
@@ -0,0 +1,27 @@
+/*
+Given a list of daily temperatures, produce a list that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+
+For example, given the list temperatures = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+*/
+
+// use stack (record index), every time compare tempratures[i] with all elements in stack, if temperatures[i] is bigger, res[s.top()] = i - s.top(), s.pop()
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> s;
+        vector<int> res(temperatures.size(), 0);
+        for(int i = 0; i < temperatures.size(); i++)
+        {
+            while(!s.empty() && temperatures[i] > temperatures[s.top()])
+            {
+                res[s.top()] = i - s.top();
+                s.pop();
+            }
+            s.push(i);
+        }
+        
+        return res;
+    }
+};
