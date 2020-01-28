b9c3f4de7584db64f74579905ab1475c3521501b
Wei <shenw.zju@gmail.com>
2020-01-27 22:14:46 -0800

    add-702

---
+++ 702-Search-in-a-sorted-array-of-unknown-size.cpp
@@ -0,0 +1,53 @@
+Given an integer array sorted in ascending order, write a function to search target in nums.  If target exists, then return its index, otherwise return -1. However, the array size is unknown to you. You may only access the array using an ArrayReader interface, where ArrayReader.get(k) returns the element of the array at index k (0-indexed).
+
+You may assume all integers in the array are less than 10000, and if you access the array out of bounds, ArrayReader.get will return 2147483647.
+
+ 
+
+Example 1:
+
+Input: array = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: array = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+ 
+
+Note:
+
+You may assume that all elements in the array are unique.
+The value of each element in the array will be in the range [-9999, 9999].
+
+// Binary search, set right = INT_MAX
+// Forward declaration of ArrayReader class.
+class ArrayReader;
+
+class Solution {
+public:
+    int search(const ArrayReader& reader, int target) {
+      int left = 0;
+      int right = INT_MAX;
+      int mid;
+      while (left + 1 < right) {
+        mid = left + (right - left) / 2;
+        if (reader.get(mid) == target) {
+          return mid;
+        } else if (reader.get(mid) < target) {
+          left = mid;
+        } else {
+          right = mid;
+        }
+      }
+      // Now left + 1 == right. Either left is the answer, or right, or -1.
+      if (reader.get(left) == target) {
+          return left;
+      } else if (reader.get(right) == target) {
+          return right;
+      }
+      
+      return -1;
+    }
+};
