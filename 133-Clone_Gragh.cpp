f40232cb0c3ed44292613cf24ed4ca5ac459ae10
Wei <shenw.zju@gmail.com>
2020-02-22 22:54:57 -0800

    update-133

--- 133-Clone_Gragh.cpp
+++ 133-Clone_Gragh.cpp
@@ -60,3 +60,49 @@ public:
         
     }
 };
+
+// New Node class defination.
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+      if (!node) return NULL;
+      unordered_map<Node*, Node*> m;
+      queue<Node*> q;
+      q.push(node);
+      m[node] = new Node(node->val);
+      while (!q.empty()) {
+          Node* cur = q.front(); q.pop();
+          for (auto neighbor : cur->neighbors) {
+              if (!m.count(neighbor)) {
+                  m[neighbor] = new Node(neighbor->val);
+                  q.push(neighbor);
+              }
+              m[cur]->neighbors.push_back(m[neighbor]);              
+          }
+      }
+      return m[node];
+    }
+};
