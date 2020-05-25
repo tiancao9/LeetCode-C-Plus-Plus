8a3c13071ab667cfdefd827e5c18fdfb3d74279a
Wei <shenw.zju@gmail.com>
2020-05-24 22:06:42 -0700

    add-658-sol3

--- 658-Find_K_Closest_Elements.cpp
+++ 658-Find_K_Closest_Elements.cpp
@@ -79,3 +79,19 @@ public:
       return res;
     }
 };
+// Sol3: binary search.
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+      int left = 0, right = arr.size() - k;
+      while (left < right) {
+          int mid = left + (right - left) / 2;
+          if (x - arr[mid] <= arr[mid + k] - x) {
+              right = mid;
+          } else {
+              left = mid+1;
+          }
+      }
+      return vector<int>(arr.begin() + left, arr.begin() + left + k);
+    }
+};
